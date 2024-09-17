d740247ccd77a352e5cd4b270d3b8a1c59b4409a
Carlosjw <carlos.ti.infor@gmail.com>
2024-09-17 14:30:09 -0300

    litleChange

---
+++ testing_personal/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <?php
+
+        $MyFirstName = "Carlos Antonio";
+        $MyLastName = "Moura Lima";
+    
+    ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo $MyFirstName . " " . $MyLastName;
+    ?>
+</body>
+</html>
