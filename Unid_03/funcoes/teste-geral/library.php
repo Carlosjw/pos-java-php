3ab41a05dd5562c7e7a6ed9663c4dc8a9e1d3b4d
Carlosjw <carlos.ti.infor@gmail.com>
2024-09-24 14:50:03 -0300

    endPersonalteste

--- Unid_03/funcoes/teste-geral/library.php
+++ Unid_03/funcoes/teste-geral/library.php
@@ -8,6 +8,57 @@
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    body{
+        width: 100dvw;
+        height: 100dwh;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .main-list {
+        list-style: none;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .sinopse-title {
+        margin-left: 20px;
+    }
+
+    .content {
+        padding: 0 18px;
+        display: none;
+        overflow: hidden;
+        background-color: #fff;
+    }
+
+    .collapsible {
+        color: #000;
+        font-weight: bold;
+        cursor: pointer;
+        padding: 18px;
+        width: 100%;
+        border: none;
+        text-align: left;
+        outline: none;
+        font-size: 15px;
+        background-color: lightgrey
+    }
+
+    .active,
+    .collapsible:hover {
+        background-color: grey;
+
+    }
+</style>
 <?php
 // CONFIGURANDO CONEXÃO COM SERVIDOR DE BANCO DE DADOS
 header("Content-Type: text/html; charset=utf-8", true);
@@ -21,7 +72,7 @@ $Biblioteca_SQL = array();
 
 //VALIDANDO CONEXÃO
 if (mysqli_connect_errno()) {
-    echo "<b>Falha</b> ao conectar no banco de dados " . "<b>" . DB . "</b>" . "Motivo: " . "<b><br>" . mysqli_connect_error() . "</b>";
+    echo "<b>Falha</b> ao conectar no banco de dados " . "<b>" . DB . "</b>" . "<br>Motivo: " . "<b><br>" . mysqli_connect_error() . "</b>";
 } else {
     echo "<b>Sucesso</b> ao conectar no banco de dados " . "<b>" . DB . "</b>!<br><br>";
 
@@ -34,7 +85,8 @@ if (mysqli_connect_errno()) {
                 "author" => $sql_livro[3],
                 "genre" => $sql_livro[4],
                 "pages" => $sql_livro[5],
-                "year" => $sql_livro[6]
+                "year" => $sql_livro[6],
+                "sinopse" => $sql_livro[7]
             );
         };
         mysqli_free_result($result);
@@ -43,11 +95,11 @@ if (mysqli_connect_errno()) {
 ?>
 
 <body>
-    <div class="w3-card w3-margin" style="width: 50%">
-        <div class="w3-container w3-padding">
+    <div class="w3-margin" style="width: 40%">
+        <div class="w3-container w3-padding w3-dark-grey w3-text-white">
             <h4>LIVROS PARA A VIDA TODA</h4>
         </div>
-        <ul class="w3-ul w3-hoverable w3-white">
+        <ul class="main-list">
             <?php
 
             for ($full_lenght = 0; $full_lenght < count($Biblioteca_SQL); $full_lenght++) {
@@ -57,22 +109,51 @@ if (mysqli_connect_errno()) {
                 $D = $Biblioteca_SQL[$full_lenght]["genre"];
                 $E = $Biblioteca_SQL[$full_lenght]["pages"];
                 $F = $Biblioteca_SQL[$full_lenght]["year"];
+                $G = $Biblioteca_SQL[$full_lenght]["sinopse"];
             ?>
-                <li class="w3-padding-16">
-                    <img src=<?php echo $A ?> alt="default-image" class="w3-left w3-margin-right" style="width: 100px">
-                    <span class="w3-large"><?php echo $B ?></span>
+                <li class="w3-light-grey w3-card" style="margin-bottom: 10px">
+                    <img src=<?php echo $A ?> alt="default-image" class="w3-left w3-margin-right" style="width: 100px;">
+                    <span class="w3-xlarge"><b><?php echo $B ?></b></span>
                     <br>
-                    <span>Autor: <?php echo $C ?></span><br>
-                    <span>Gênero: <?php echo $D ?></span><br>
-                    <span>Páginas: <?php echo $E ?></span><br>
-                    <span>Ano: <?php echo $F ?></span><br><br>
+                    <span><b>Autor:</b> <?php echo $C ?></span><br>
+                    <span><b>Gênero:</b> <?php echo $D ?></span><br>
+                    <span><b>Páginas:</b> <?php echo $E ?></span><br>
+                    <span><b>Ano:</b> <?php echo $F ?></span><br><br>
+                    <button type="button" class="collapsible">Sinopse:</button>
 
+                    <div class="content w3-card" >
+                        <p class="w3-padding" style="text-align:justify; text-indent:2em">
+                            <?php echo $G ?>
+                        </p>
+                    </div>
                 </li>
+
             <?php
             }
             ?>
         </ul>
     </div>
+
+    <footer class="w3-container w3-dark-grey w3-margin-top w3-cell-row">
+
+    </footer>
+
+    <script>
+        var coll = document.getElementsByClassName("collapsible");
+        var i;
+
+        for (i = 0; i < coll.length; i++) {
+            coll[i].addEventListener("click", function() {
+                this.classList.toggle("active");
+                var content = this.nextElementSibling;
+                if (content.style.display === "block") {
+                    content.style.display = "none";
+                } else {
+                    content.style.display = "block";
+                }
+            });
+        }
+    </script>
 
 </body>
 
