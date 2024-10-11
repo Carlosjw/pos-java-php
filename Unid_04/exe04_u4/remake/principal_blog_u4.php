b40dae9619cd6205e8b8fa2dc3550905b148df84
Carlosjw <carlos.ti.infor@gmail.com>
2024-10-11 15:36:00 -0300

    UlittleChanges

--- Unid_04/exe04_u4/remake/principal_blog_u4.php
+++ Unid_04/exe04_u4/remake/principal_blog_u4.php
@@ -8,7 +8,7 @@ include("head.php");
 <body>
     <div class="w3-card w3-container w3-padding" style="width: 480px;">
         <?php
-        include("createPhone.php")
+        include("createPhone.php");
         ?>
     </div>
 </body>
